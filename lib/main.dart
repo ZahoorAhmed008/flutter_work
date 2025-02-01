0901da6a2ad4b439588136c467c38db30789adf6
Zahoor Ahmed <za0389188@gmail.com>
2025-02-02 01:29:28 +0500

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_work/HomePage.dart';
+import 'package:flutter_work/Pages/HomePage.dart';
+import 'package:flutter_work/Pages/loginpage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     int days = 30;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-   home: Homepage(),
+      //  home: Homepage(),
+      themeMode: ThemeMode.dark, // Ensure this is present
+      theme: ThemeData(
+          primarySwatch: Colors.cyan), // Define light theme explicitly
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => Loginpage(),
+        "/Loginpage": (context) => Homepage()
+      },
     );
   }
 }
